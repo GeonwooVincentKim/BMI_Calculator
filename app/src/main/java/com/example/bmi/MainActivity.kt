bf715461e35aef1cb6a4d2b38716ec0c8477bce4
GeonwooVincentKim <kdsnop@gmail.com>
2021-09-07 16:37:53 +0900

    [Feature] - Designed when user successfully logged in

--- app/src/main/java/com/example/bmi/MainActivity.kt
+++ app/src/main/java/com/example/bmi/MainActivity.kt
@@ -1,10 +1,12 @@
 package com.example.bmi
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.util.Log
 import android.widget.Button
 import android.widget.EditText
+import android.widget.Toast
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -29,10 +31,19 @@ class MainActivity : AppCompatActivity() {
         resultButton.setOnClickListener{
             Log.d("MainActivity", "ResultButton 이 클릭되었습니다")
 
+            if(heightEditText.text.isEmpty() || weightEditText.text.isEmpty()){
+                // If some values in the text are empty
+                Toast.makeText(this, "빈 값이 있습니다.", Toast.LENGTH_SHORT).show()
+                return@setOnClickListener
+            }
+
             val height: Int = heightEditText.text.toString().toInt();
             val weight: Int = weightEditText.text.toString().toInt();
 
             Log.d("MainActivity", "height : $height - weight : $weight")
+
+            val intent = Intent(this, ResultActivity::class.java)
+            startActivity(intent)
         };
     }
 }
