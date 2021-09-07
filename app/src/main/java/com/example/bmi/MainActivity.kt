b906bf1cb4f972625ba87255a42d95a6802d8897
GeonwooVincentKim <kdsnop@gmail.com>
2021-09-07 16:12:41 +0900

    [Feature] - Make the button and textArea can input and send the data

--- app/src/main/java/com/example/bmi/MainActivity.kt
+++ app/src/main/java/com/example/bmi/MainActivity.kt
@@ -2,10 +2,37 @@ package com.example.bmi
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
+import android.widget.Button
+import android.widget.EditText
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
+        /*
+        * Show the title of Application -> Header
+        * => This is the override function from `AppCompatActivity`
+        *    If you delete this code, it will makes this application not working
+        * */
         super.onCreate(savedInstanceState)
+
+        /*
+        * Show the contents of Application -> Body
+        * => If you delete this code, it will makes this application not display
+        *    the body part of it
+        * */
         setContentView(R.layout.activity_main)
+
+        val heightEditText: EditText = findViewById(R.id.heightEditText); // Variable allocation style
+        val weightEditText = findViewById<EditText>(R.id.weightEditText); // Generic Style
+
+        val resultButton: Button = findViewById(R.id.resultButton);
+        resultButton.setOnClickListener{
+            Log.d("MainActivity", "ResultButton 이 클릭되었습니다")
+
+            val height: Int = heightEditText.text.toString().toInt();
+            val weight: Int = weightEditText.text.toString().toInt();
+
+            Log.d("MainActivity", "height : $height - weight : $weight")
+        };
     }
 }
